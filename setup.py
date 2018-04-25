c9dd415abed05b85f47bc4cb38e589704b962046
Shubham Chandel <shubham.zeez@gmail.com>
2018-04-25 16:13:52 +0530

    v1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='torchsummary',
-	version='1.0',
+	version='1.1',
 	description='Model summary in PyTorch similar to `model.summary()` in Keras',
 	url='https://github.com/sksq96/pytorch-summary',
 	author='Shubham Chandel @sksq96',
