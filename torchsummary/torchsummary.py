87796591fcdb7110081c3cd02538b8a6d554b35c
Shubham Chandel <shubham.zeez@gmail.com>
2018-04-25 12:11:12 +0530

    Bugfix: None bias error

--- torchsummary/torchsummary.py
+++ torchsummary/torchsummary.py
@@ -1,8 +1,9 @@
-import torch as th
+import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
 from collections import OrderedDict
+
 
 def summary(model, input_size):
         def register_hook(module):
@@ -14,38 +15,37 @@ def summary(model, input_size):
                 summary[m_key] = OrderedDict()
                 summary[m_key]['input_shape'] = list(input[0].size())
                 summary[m_key]['input_shape'][0] = -1
-                summary[m_key]['output_shape'] = list(output.size())
-                summary[m_key]['output_shape'][0] = -1
+                if isinstance(output, (list,tuple)):
+                    summary[m_key]['output_shape'] = [[-1] + list(o.size())[1:] for o in output]
+                else:
+                    summary[m_key]['output_shape'] = list(output.size())
+                    summary[m_key]['output_shape'][0] = -1
 
                 params = 0
                 if hasattr(module, 'weight'):
-                    params += th.prod(th.LongTensor(list(module.weight.size())))
-                    if module.weight.requires_grad:
-                        summary[m_key]['trainable'] = True
-                    else:
-                        summary[m_key]['trainable'] = False
-                if hasattr(module, 'bias'):
-                    params +=  th.prod(th.LongTensor(list(module.bias.size())))
+                    params += torch.prod(torch.LongTensor(list(module.weight.size())))
+                    summary[m_key]['trainable'] = module.weight.requires_grad
+                if hasattr(module, 'bias') and hasattr(module.bias, 'size'):
+                    params +=  torch.prod(torch.LongTensor(list(module.bias.size())))
                 summary[m_key]['nb_params'] = params
                 
-            if not isinstance(module, nn.Sequential) and \
-               not isinstance(module, nn.ModuleList) and \
-               not (module == model):
+            if (not isinstance(module, nn.Sequential) and 
+               not isinstance(module, nn.ModuleList) and 
+               not (module == model)):
                 hooks.append(module.register_forward_hook(hook))
                 
-        if th.cuda.is_available():
-            dtype = th.cuda.FloatTensor
+        if torch.cuda.is_available():
+            dtype = torch.cuda.FloatTensor
         else:
-            dtype = th.FloatTensor
+            dtype = torch.FloatTensor
         
         # check if there are multiple inputs to the network
         if isinstance(input_size[0], (list, tuple)):
-            x = [Variable(th.rand(1,*in_size)).type(dtype) for in_size in input_size]
+            x = [Variable(torch.rand(1,*in_size)).type(dtype) for in_size in input_size]
         else:
-            x = Variable(th.rand(1,*input_size)).type(dtype)
+            x = Variable(torch.rand(1,*input_size)).type(dtype)
             
             
-        # print(x.shape)
         # print(type(x[0]))
         # create properties
         summary = OrderedDict()
@@ -53,6 +53,7 @@ def summary(model, input_size):
         # register hook
         model.apply(register_hook)
         # make a forward pass
+        # print(x.shape)
         model(x)
         # remove these hooks
         for h in hooks:
@@ -65,7 +66,7 @@ def summary(model, input_size):
         total_params = 0
         trainable_params = 0
         for layer in summary:
-            ## input_shape, output_shape, trainable, nb_params
+            # input_shape, output_shape, trainable, nb_params
             line_new = '{:>20}  {:>25} {:>15}'.format(layer, str(summary[layer]['output_shape']), summary[layer]['nb_params'])
             total_params += summary[layer]['nb_params']
             if 'trainable' in summary[layer]:
