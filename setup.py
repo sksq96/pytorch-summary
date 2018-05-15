010a200c151ffeae325df42e493f1ef1ba9cddff
Bond-SYSU <374579557@qq.com>
2018-05-15 16:29:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='torchsummary',
-	version='1.1',
-	description='Model summary in PyTorch similar to `model.summary()` in Keras',
-	url='https://github.com/sksq96/pytorch-summary',
-	author='Shubham Chandel @sksq96',
-	author_email='shubham.zeez@gmail.com',
+	name='dubugsummary',
+	version='0.1',
+	description='Model debug and summary in PyTorch',
+	url='https://github.com/Bond-SYSU/pytorch-summary',
+	author='Shubham Chandel @',
+	author_email='huangdb3@mail2.sysu.edu.cn',
 	packages=['torchsummary'],
 )
