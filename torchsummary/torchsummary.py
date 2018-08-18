8f4d61910a91d769dad1a4701fd2d13df8b7fd46
赵航 <zh13@meitu.com>
2018-08-18 23:39:41 +0800

    add calc conv2d&linear MACC

--- torchsummary/torchsummary.py
+++ torchsummary/torchsummary.py
@@ -26,6 +26,17 @@ def summary(model, input_size, batch_size=-1, device="cuda"):
                 summary[m_key]["output_shape"] = list(output.size())
                 summary[m_key]["output_shape"][0] = batch_size
 
+            if isinstance(module,nn.Conv2d):
+                k = module.kernel_size
+                g = module.groups
+                in_s = summary[m_key]["input_shape"]
+                out_s = summary[m_key]["output_shape"]
+                summary[m_key]["macc"] = (k[0] * k[1] * in_s[1] // g * out_s[-1] * out_s[-2] * out_s[-3] // g) * g
+            elif isinstance(module,nn.Linear):
+                summary[m_key]["macc"] = module.in_features * module.out_features
+            else:
+                summary[m_key]["macc"] = 0
+
             params = 0
             if hasattr(module, "weight") and hasattr(module.weight, "size"):
                 params += torch.prod(torch.LongTensor(list(module.weight.size())))
@@ -74,20 +85,23 @@ def summary(model, input_size, batch_size=-1, device="cuda"):
     for h in hooks:
         h.remove()
 
-    print("----------------------------------------------------------------")
-    line_new = "{:>20}  {:>25} {:>15}".format("Layer (type)", "Output Shape", "Param #")
+    print("--------------------------------------------------------------------------------------------")
+    line_new = "{:>20}  {:>25} {:>15} {:>20}".format("Layer (type)", "Output Shape", "Param #", "MACC #")
     print(line_new)
-    print("================================================================")
+    print("============================================================================================")
     total_params = 0
     total_output = 0
     trainable_params = 0
+    total_macc = 0
+    for layer in summary:
+        total_macc += summary[layer]["macc"]
     for layer in summary:
         # input_shape, output_shape, trainable, nb_params
-        line_new = "{:>20}  {:>25} {:>15}".format(
+        line_new = "{:>20}  {:>25} {:>15} {:>20}".format(
             layer,
             str(summary[layer]["output_shape"]),
             "{0:,}".format(summary[layer]["nb_params"]),
-        )
+            ("{0:.2%}".format(summary[layer]["macc"] / total_macc) if summary[layer]["macc"] else '-'))
         total_params += summary[layer]["nb_params"]
         total_output += np.prod(summary[layer]["output_shape"])
         if "trainable" in summary[layer]:
@@ -101,14 +115,15 @@ def summary(model, input_size, batch_size=-1, device="cuda"):
     total_params_size = abs(total_params.numpy() * 4. / (1024 ** 2.))
     total_size = total_params_size + total_output_size + total_input_size
 
-    print("================================================================")
+    print("============================================================================================")
     print("Total params: {0:,}".format(total_params))
+    print("Total MACC: {0:,}".format(total_macc))
     print("Trainable params: {0:,}".format(trainable_params))
     print("Non-trainable params: {0:,}".format(total_params - trainable_params))
-    print("----------------------------------------------------------------")
+    print("--------------------------------------------------------------------------------------------")
     print("Input size (MB): %0.2f" % total_input_size)
     print("Forward/backward pass size (MB): %0.2f" % total_output_size)
     print("Params size (MB): %0.2f" % total_params_size)
     print("Estimated Total Size (MB): %0.2f" % total_size)
-    print("----------------------------------------------------------------")
+    print("--------------------------------------------------------------------------------------------")
     # return summary
