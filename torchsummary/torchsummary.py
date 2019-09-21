a52f625b1172b58e91585666c013e2baa61d2978
Apple <apple@Adithya.local>
2019-09-22 03:16:03 +0900

    multi-gpu compatible: torch-summary for cuda:1 and so on

--- torchsummary/torchsummary.py
+++ torchsummary/torchsummary.py
@@ -104,7 +104,7 @@ def summary(model, input_size, batch_size=-1, device="cuda"):
                     trainable_params += summary[layer]["nb_params"]
             print(line_new)
 
-      # assume 4 bytes/number (float on cuda).
+        # assume 4 bytes/number (float on cuda).
         total_input_size = abs(np.prod(input_size) * batch_size * 4. / (1024 ** 2.))
         total_output_size = abs(2. * total_output * 4. / (1024 ** 2.))  # x2 for gradients
         total_params_size = abs(total_params.numpy() * 4. / (1024 ** 2.))
